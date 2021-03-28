8dc3ad9e763da8500c3bcff37bf605d8297d38be
Tejas Tawde <64289960+tejastawde@users.noreply.github.com>
2021-03-29 03:23:04 +0530

    Added custom email model
    Added custom email model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,5 +13,24 @@ class ModelTests(TestCase):
             password=password
         )
 
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for new user is normalized"""
+        email = 'tejastawde96@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'tejastawde96@gmail.com', 'test123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
